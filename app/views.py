719888593d46f3634553e2d3dbc3058a9a28c9f2
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2017-05-27 10:55:01 -0400

    Add database interactions

--- app/views.py
+++ app/views.py
@@ -1,32 +1,37 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, jsonify
+from app import app, db, models
 from .forms import MessageForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
-    posts = [  # fake array of posts
-        {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    messages = models.Message.query.all()
     return render_template("index.html",
                            title='Home',
                            user=user,
-                           posts=posts)
+                           messages=messages)
 
 @app.route('/messages', methods=['GET', 'POST'])
 def messages():
     form = MessageForm()
     if form.validate_on_submit():
-        flash('Message sent to' + str(form.recipient.data))
+        m = models.Message(
+            recipient=form.recipient.data,
+            message=form.message.data
+        )
+        db.session.add(m)
+        db.session.commit()
+        flash('Message sent to ' + str(form.recipient.data))
         return redirect('/index')
     return render_template('message.html',
                             title='Send Message',
                             form=form)
+
+@app.route('/get_data')
+def get_data():
+    messages = models.Message.query.all()
+    return jsonify([
+        {'recipient': m.recipient, 'message': m.message}
+        for m in messages
+    ])

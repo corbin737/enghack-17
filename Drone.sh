d2c45e4339c5555724a9964231d10b7084d86d34
Quinlan Sykora <quinlan.sykora@robotics.utias.utoronto.ca>
2017-05-27 16:25:45 -0400

    yup

--- Drone.sh
+++ Drone.sh
@@ -3,4 +3,4 @@
 networksetup -setairportnetwork en1 goto_http://169.254.165.146:5000
 sleep 20
 curl -X GET http://169.254.109.1:5000/get_data > data.json
-
+curl -d message.json http://169.254.109.1:5000/post_data

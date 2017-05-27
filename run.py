51e07166ce475b6ab1851ee5a31a983d2faa3fb3
Corbin McElhanney <corbin.mcelhanney@gmail.com>
2017-05-27 03:36:41 -0400

    Change to non-local server

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import app
 import threading, webbrowser
 threading.Timer(1.25, lambda: webbrowser.open('http://127.0.0.1:5000')).start()
 
-app.run(debug=True)
+app.run(debug=False, host='0.0.0.0', port='80')

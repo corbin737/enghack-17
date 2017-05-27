6171f3ff61348afdac626d2e65ab5abd48f26443
Anna Verkhovskaya <verkhovskaya.anna@gmail.com>
2017-05-27 15:17:33 -0400

    work.

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from app import app
 
 import threading, webbrowser
-threading.Timer(1.25, lambda: webbrowser.open('http://127.0.0.1:5000')).start()
+threading.Timer(1.25, lambda: webbrowser.open('http://0.0.0.0:5000')).start()
 
-# app.run(debug=False, host='0.0.0.0')
-app.run(debug=True)
+app.run(debug=False, host='0.0.0.0')
+# app.run(debug=True)

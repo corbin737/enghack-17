d4d37008f5b2578cb41125db760879638acc5e8a
Quinlan Sykora <quinlan.sykora@robotics.utias.utoronto.ca>
2017-05-27 17:08:58 -0400

    yay

--- Drone.sh
+++ Drone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-networksetup -setairportnetwork en1 goto_http://169.254.165.146:5000
+networksetup -setairportnetwork en1 goto_http://10.20.7.49:5000
 sleep 20
 curl -X GET http://169.254.109.1:5000/get_data > data.json
-curl -d message.json http://169.254.109.1:5000/post_data
+curl -H "Content-Type: application/text" POST -d "Hello everyone, this is the Red Cross. We are sending you this message to inform you that we will be saving your butts within the next hour so just hold on and try not to scream too much when a drone comes to pick up your message" http://169.254.109.1:5000/post_data
